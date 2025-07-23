c1905f41c52664f96efeccafb9ae5eec14045da1
galyna-k <hkzlvs@gmail.com>
2025-07-23 10:38:39 +0100

    Update script.js to render episode count in main element
    - create or clear main element in setup;
    - add paragraph with number of episodes to main.

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 //You can edit ALL of the code here
 function setup() {
   const allEpisodes = getAllEpisodes();
+  createMain();
   makePageForEpisodes(allEpisodes);
 }
 
-function makePageForEpisodes(episodeList) {
+// Creates the main element and appends it to the root element
+function createMain() {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  let mainElem = rootElem.querySelector("main");
+
+  if (!mainElem) {
+    mainElem = document.createElement("main");
+    mainElem.className = "container";
+    rootElem.appendChild(mainElem);
+  }
+
+  mainElem.innerHTML = "";
+}
+
+function makePageForEpisodes(episodeList) {
+  const mainElem = document.querySelector("main");
+  const countText = document.createElement("p");
+  countText.textContent = `Got ${episodeList.length} episode(s)`;
+  mainElem.prepend(countText);
 }
 
 window.onload = setup;
