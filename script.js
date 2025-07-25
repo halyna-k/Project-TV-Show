cc77df017429b3b60a996beb2829103712f984c8
Emilianouz <135679131+Emilianouz@users.noreply.github.com>
2025-07-25 15:34:00 +0100

    Search by input and Episode selector implemented. Adding nav. Changes on HTML, JS and Styles.

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function setup() {
   createMain();
   makePageForEpisodes(allEpisodes);
   footer();
+  createNav();
 }
 
 // Creates the main element and appends it to the root element
@@ -35,16 +36,13 @@ function createEpisodeContainer() {
 
   return container;
 }
-
 // Function to pad numbers with leading zeros
 const pad = (n) => {
   if (typeof n !== "number" || isNaN(n)) return "00";
   return n.toString().padStart(2, "0");
 };
-
 // Template for episode cards
 const template = document.getElementById("episode-card-template");
-
 // Function to create the episode card
 function createEpisodeCard(episode) {
   const episodeCard = template.content.cloneNode(true);
@@ -76,15 +74,86 @@ function footer() {
 
 function makePageForEpisodes(episodeList) {
   const mainElem = document.querySelector("main");
-  const countText = document.createElement("h1");
-  countText.textContent =
-    episodeList.length === 0
-      ? "No episodes found"
-      : `Now is available ${episodeList.length} TV shows`;
-  mainElem.prepend(countText);
-
+    //countText where here
   const episodeContainer = createEpisodeContainer();
   episodeContainer.append(...episodeList.map(createEpisodeCard));
 }
+//  *********    E M I L I A N O code from here
+function createNav() {
+  const navElem = document.querySelector("nav");
+  const allEpisodes = getAllEpisodes();
+  // create select element
+  let selectSearch = document.createElement("select");
+  selectSearch.name = "select-search";
+  selectSearch.id = "select-search";
+  listEpisodesToSelect(selectSearch,allEpisodes);
+  navElem.appendChild(selectSearch);
+  selectSearch.addEventListener("change",searchByList); // event select
+  //create search input
+  let inputSearch = document.createElement("input");
+  inputSearch.id = "input-search";
+  inputSearch.type = 'text';
+  inputSearch.placeholder = "Search episodes... "
+  navElem.appendChild(inputSearch);
+  inputSearch.addEventListener("keyup",searchByInput); // event input
+  //create counter elements
+  const countText = document.createElement("h3");
+  countText.id = "counter-episodes";
+  countText.textContent = counterElements(allEpisodes,allEpisodes);
+  navElem.appendChild(countText);
+}
+// element counter
+function counterElements(allEpisodes,filterEpisodes){
+  let countText = "";
+  const numAllEpisodes = allEpisodes.length
+  const numFilterEpisodes = filterEpisodes.length
+  countText = `Displaying ${numFilterEpisodes} / ${numAllEpisodes} episodes`;
+  return countText;
+}
+// this function create the list of episodes for the select option.
+function listEpisodesToSelect(selectSearch,allEpisodes){
+    // add value to show all episodes
+  const optionAll = document.createElement("option");
+  optionAll.value = '*'
+  optionAll.textContent = "Show All"
+  selectSearch.appendChild(optionAll);
+
+  listEpisodes = allEpisodes.map((episode) => {
+    const option = document.createElement("option");
+    option.value = episode.id;
+    option.textContent = `S${pad(episode.season)}E${pad(episode.number)} - ${episode.name}`;
+    return option;
+  });
+  listEpisodes.forEach((option) => selectSearch.appendChild(option)); 
+}
+
+//function after update list to filter
+function searchByList(){
+  const episodeId = Number(document.getElementById("select-search").value);
+  const allEpisodes = getAllEpisodes();
+  let episodeTarget = allEpisodes;
+  if (!episodeId){
+      makePageForEpisodes(allEpisodes);
+    } else {
+      episodeTarget = allEpisodes.filter( (ep) => ep.id === episodeId);
+      makePageForEpisodes(episodeTarget);  
+    }
+  //update counter episodes
+  let countText = document.getElementById("counter-episodes");
+  countText.textContent = counterElements(allEpisodes,episodeTarget);
+}
+// function after update input-search
+function searchByInput(){
+  const textSearched = document.getElementById("input-search").value;
+  const allEpisodes = getAllEpisodes();
+  const episodesResult = allEpisodes.filter((ep) => 
+      ep.name.toLowerCase().includes(textSearched) || ep.summary.toLowerCase().includes(textSearched)
+  );/// 
+  makePageForEpisodes(episodesResult);
+  // update counter episodes
+  let countText = document.getElementById("counter-episodes");
+  countText.textContent = counterElements(allEpisodes,episodesResult);
+}
+
 
 window.onload = setup;
